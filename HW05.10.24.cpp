cf28f3a219fcceb596c2ddafc03246f2f446d109
SashkoSt <oleksandrstefaniyk@gmail.com>
2024-10-05 11:05:45 +0300

    Завлання 2

--- HW05.10.24.cpp
+++ HW05.10.24.cpp
@@ -1,16 +1,63 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <Windows.h>
+//#include <cstdlib>
+//#include <ctime>
+//using namespace std;
+//
+//void rowtodel(int ar[4][4], int& rows, int& cols, int rowdel) {
+//	for (int i = rowdel; i < rows - 1; i++) {
+//		for (int j = 0; j < cols; j++) {
+//			ar[i][j] = ar[i+1][j];
+//		}
+//	}
+//	rows--;
+//}
+//int main()
+//{
+//	SetConsoleCP(1251);
+//	SetConsoleOutputCP(1251);
+//	srand(time(0));
+//
+//	int cols = 4;
+//	int rows = 4;
+//	int ar[4][4];
+//	
+//
+//	for (int i = 0; i < rows; i++) {
+//		for (int j = 0; j < cols; j++) {
+//			ar[i][j] = rand() % 100;
+//			cout << ar[i][j]<< ' ';
+//		}
+//		cout << endl;
+//	}
+//
+//	int rowdel;
+//	cout << "Введіть рядок який ви хочете видалити" << endl;
+//	cin >> rowdel;
+//	rowdel--;
+//	rowtodel(ar, rows, cols, rowdel);
+//
+//	for (int i = 0; i < rows; i++) {
+//		for (int j = 0; j < cols; j++) {
+//			cout << ar[i][j] << ' ';
+//		}
+//		cout << endl;
+//	}
+//
+//}
+//
+#include <iostream>
 #include <Windows.h>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
-void rowtodel(int ar[4][4], int& rows, int& cols, int rowdel) {
-	for (int i = rowdel; i < rows - 1; i++) {
+void transport(int ar[4][4], int ar2[4][4], int rows, int cols) {
+	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			ar[i][j] = ar[i+1][j];
+			ar2[j][i] = ar[i][j];
 		}
 	}
-	rows--;
 }
 int main()
 {
@@ -31,18 +78,15 @@ int main()
 		cout << endl;
 	}
 
-	int rowdel;
-	cout << "Введіть рядок який ви хочете видалити" << endl;
-	cin >> rowdel;
-	rowdel--;
-	rowtodel(ar, rows, cols, rowdel);
-
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j++) {
-			cout << ar[i][j] << ' ';
+	int ar2[4][4];
+	transport(ar, ar2, rows, cols);
+	cout << "Перевернута матриця" << endl;
+	for (int i = 0; i < cols; i++) {
+		for (int j = 0; j < rows; j++) {
+			cout << ar2[i][j] << ' ';
 		}
 		cout << endl;
 	}
-
+	return 0;
 }
 
